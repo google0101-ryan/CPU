b511dee5a8033f2c320b3a8c244c36deb419c815
hello01-debug <56547135+hello01-debug@users.noreply.github.com>
2023-04-21 19:37:17 +0000

    Added basic framework for PCI devices

--- pci/DramControllerQ35.cpp
+++ pci/DramControllerQ35.cpp
@@ -9,6 +9,8 @@ uint16_t DramController::ReadConfigSpace16(uint8_t offs)
 {
     switch (offs)
     {
+    case 0x02:
+        return 0x29C0; // Q35 Northbridge
     default:
         printf("[Q35/DramController]: Read from unknown config space register 0x%02x\n", offs);
         exit(1);
--- pci/PCI.cpp
+++ pci/PCI.cpp
@@ -10,7 +10,7 @@ PCIBus::PCIBus()
 
 void PCIBus::RegisterDevice(int bus, int dev, int func, PCIDevice *device)
 {
-    uint32_t addr = (1 << 31) | (bus << 16) | (dev << 11) | (func << 8);
+    uint32_t addr = (bus << 16) | (dev << 11) | (func << 8);
     devices[dev] = device;
 }
 
@@ -31,13 +31,13 @@ uint16_t PCIBus::Read16(uint16_t port)
     switch (port)
     {
     case 0xcfc ... 0xcff:
-        if (!devices[pciAddr & 0xFFFFFF00])
+        if (!devices[pciAddr & 0x7FFFFF00])
         {
-            printf("ERROR: Can't read from non-existant PCI device 0x%08x\n", pciAddr & 0xFFFFFF00);
+            printf("ERROR: Can't read from non-existant PCI device 0x%08x\n", pciAddr & 0x7FFFFF00);
             exit(1);
         }
 
-        return devices[pciAddr & 0xFFFFFF00]->ReadConfigSpace16((pciAddr & 0xFF) + (port-0xcfc));
+        return devices[pciAddr & 0x7FFFFF00]->ReadConfigSpace16((pciAddr & 0xFF) + (port-0xcfc));
     default:
         printf("Unhandled read from PCI port 0x%04x\n", port);
         exit(1);
